6e2a38e6eb69da35412c938a2e6773a4776ba057
gauravkumar <gthakurx@gmail.com>
2024-01-07 18:22:43 +0530

    adding UUID as type

--- src/main/java/com/productservice/productservice/Models/BaseModel.java
+++ src/main/java/com/productservice/productservice/Models/BaseModel.java
@@ -1,10 +1,18 @@
 package com.productservice.productservice.Models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.UUID;
 
 @MappedSuperclass
 public class BaseModel {
     @Id
-    private long id;
+    @GeneratedValue(generator = "uuidgenerator")
+    @GenericGenerator(name = "uuidgenerator",strategy = "uuid2")
+    @Column(name="id",columnDefinition = "binary(16)",nullable = false,updatable = false)
+    private UUID id;
 }
