81df4222d4c5ae55f7aac0dfefc651fd390144b0
gauravkumar <gthakurx@gmail.com>
2024-01-07 17:59:15 +0530

    Commiting database creation and dependency Changes

--- src/main/java/com/productservice/productservice/Models/BaseModel.java
+++ src/main/java/com/productservice/productservice/Models/BaseModel.java
@@ -1,5 +1,10 @@
 package com.productservice.productservice.Models;
 
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class BaseModel {
+    @Id
     private long id;
 }
--- src/main/java/com/productservice/productservice/Models/Product.java
+++ src/main/java/com/productservice/productservice/Models/Product.java
@@ -1,17 +1,20 @@
 package com.productservice.productservice.Models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Product extends BaseModel {
     //private int id;
     private String title;
     private String Description;
     private int price;
     private String imageurl;
+    @ManyToOne
     private Category category;
-    private double rating;
 
 }
